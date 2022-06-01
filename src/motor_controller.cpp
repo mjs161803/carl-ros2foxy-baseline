edbb86c966abf77376e61acf1bf059c9b26b657a
mjs161803 <mike@agronica.farm>
2022-06-01 05:26:34 -0400

    added code for motor_controller node to receive twist messages

--- src/motor_controller.cpp
+++ src/motor_controller.cpp
@@ -8,6 +8,11 @@
 #include "std_msgs/msg/string.hpp"
 #include "carl_interfaces/msg/arduino_command_a.hpp"
 
+#define WHEEL_RADIUS 4.005
+#define WHEEL_BASE 160.0
+#define DRIVE_RPM 12000
+#define TURN_RPM 7000
+
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
@@ -15,15 +20,44 @@ class MotorController : public rclcpp::Node {
 	public:
 		MotorController() : Node("motor_controller") {
 			motor_command_publisher_ = this->create_publisher<carl_interfaces::msg::ArduinoCommandA>("motor_commands", 10);
-			motor_command_timer_ = this->create_wall_timer(10s, std::bind(&MotorController::motor_command_callback, this));
-		
+			teleop_subscription_ = this->create_subscription<geometry_msgs::msg::Twist>("carl_teleop_commands", 10, std::bind(&MotorController::motor_command_callback, this, _1));
 		}
 	private: 
-		void motor_command_callback() {
-			short int l_rpm {10000};
-			short int r_rpm {-10000};
-			unsigned int num_l_ticks {747};
-			unsigned int num_r_ticks {747};
+		void motor_command_callback(const geometry_msgs::msg::Twist::SharedPtr twist_msg) const {
+			// CARL acts as its own frame of reference, with +/- linear.x correlating to forward and backward.
+			// +/- angular.z indicates a rotation left (-) or right (+) from current azimuth
+			// Either linear.x or angular.z should be non-zero for each callback, but not both.
+
+			double delta_x = twist_msg->linear.x; // forward or backware x centimeters
+			double delta_theta = twist_msg->angular.z; // rotate left or right by z degrees
+
+			// Calculate ticks from twist message parameters
+			unsigned int num_l_ticks;
+			unsigned int num_r_ticks;
+			short int l_rpm;
+			short int r_rpm;
+
+			if (delta_x != 0.0) { // 	Twist command was to move forward or backward
+				num_l_ticks = (unsigned int)((abs(delta_x) / (6.2832*WHEEL_RADIUS))*746.94);		
+				num_r_ticks = num_l_ticks;
+				l_rpm = DRIVE_RPM;
+				r_rpm = DRIVE_RPM;
+				if (delta_x < 0.0) {
+					l_rpm *= -1;
+					r_rpm *= -1;
+				}
+			} else { // 			Twist command was to rotate left or right
+				double arc_dist = (WHEEL_BASE / 2.0)*(abs(delta_theta) * (6.2832 / 360.0));
+			       	num_l_ticks = (unsigned int)((arc_dist / (6.2832*WHEEL_RADIUS))*746.94);	
+				num_r_ticks = num_l_ticks;
+				l_rpm = TURN_RPM;
+				r_rpm = TURN_RPM;
+				if (delta_theta < 0.0) {
+					l_rpm *= -1;
+				} else {
+					r_rpm *= -1;
+				}
+			}
 
 			auto message = carl_interfaces::msg::ArduinoCommandA();
 			message.left_rpm = l_rpm;
@@ -35,8 +69,8 @@ class MotorController : public rclcpp::Node {
 			motor_command_publisher_->publish(message);
 		}
 
-		rclcpp::TimerBase::SharedPtr motor_command_timer_;
 		rclcpp::Publisher<carl_interfaces::msg::ArduinoCommandA>::SharedPtr motor_command_publisher_;
+		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr teleop_subscription_;
 };
 
 int main(int argc, char* argv[]) {
